5b7efe9aef82908192556da06270f413315541cb
Colin <colin.diesh@gmail.com>
2017-02-10 16:29:27 -0500

    Change to shiftwidth 4

--- app.R
+++ app.R
@@ -4,114 +4,126 @@ library(Rsamtools)
 
 
 shinyApp(
-  ui = fluidPage(
-    titlePanel("shinyorthologs"),
+    ui = fluidPage(
+        titlePanel("shinyorthologs"),
+        tabsetPanel(
+            tabPanel("Genes",
 
-    # Create a new Row in the UI for selectInputs
-    fluidRow(
-      column(4, uiOutput("species")),
-      column(4, textInput("gene", "Gene: "))
+                # Create a new Row in the UI for selectInputs
+                fluidRow(
+                    column(4, uiOutput("species")),
+                    column(4, textInput("gene", "Gene: "))
+                ),
+
+                fluidRow(
+                    DT::dataTableOutput("table")
+                ),
+
+                fluidRow(
+                    h2("Gene information"),
+                    column(4, uiOutput("row"))
+                )
+            ),
+            tabPanel("Orthologs",
+                fluidRow(
+                    h2("Ortholog information"),
+                    column(4, uiOutput("row"))
+                )
+            )
+        )
     ),
 
-    fluidRow(
-      DT::dataTableOutput("table")
-    ),
-
-    fluidRow(
-      h2("Gene information"),
-      column(4, uiOutput("row"))
-    )
-  ),
-
-  server = function(input, output, session) {
-    geneData = reactive({
-      fread('data/genes.csv')
-    })
-    transcriptData = reactive({
-      fread('data/transcripts.csv')
-    })
-    speciesData = reactive({
-      fread('data/species.csv')
-    })
-
-    orthogroupData = reactive({
-      fread('data/orthologs.csv')
-    })
-
-    observe({
-      query <- parseQueryString(session$clientData$url_search)
-      for (i in 1:(length(reactiveValuesToList(input)))) {
-        nameval = names(reactiveValuesToList(input)[i])
-        valuetoupdate = query[[nameval]]
-
-        if (!is.null(query[[nameval]])) {
-          if (is.na(as.numeric(valuetoupdate))) {
-            updateTextInput(session, nameval, value = valuetoupdate)
-          }
-          else {
-            updateTextInput(session, nameval, value = as.numeric(valuetoupdate))
-          }
-        }
-      }
-    })
-    geneTable = reactive({
-      data = geneData()
-      species = speciesData()
-      if (is.null(input$species)) {
-         return(NULL)
-      }
-      if (input$species != "All") {
-        ss = species[species$name == input$species, ]$shortName
-        data = data[data$species == ss, ]
-      }
-      if (input$gene != "") {
-        query = sprintf("select * from data where id LIKE '%%%s%%'", input$gene)
-        data = sqldf(query)
-      }
-      data
-    })
-    orthologTable = reactive({
-      data = orthologData()
-      species = speciesData()
-      if (is.null(input$species)) {
-         return(NULL)
-      }
-      if (input$species != "All") {
-        ss = species[species$name == input$species, ]$shortName
-        data = data[data$species == ss, ]
-      }
-      if (input$gene != "") {
-        query = sprintf("select * from data where id LIKE '%%%s%%'", input$gene)
-        data = sqldf(query)
-      }
-      data
-    })
-    output$table = DT::renderDataTable(geneTable(), selection = 'single')
-
-
-    output$species = renderUI({
-      data = speciesData()
-      selectInput("species", "Species:", c("All", data$name))
-    })
-
-
-    output$row = renderTable({
-      if (!is.null(input$table_rows_selected)) {
-        data = geneTable()
-        species = speciesData()
-        transcripts = transcriptData()
-
-        row = data[input$table_rows_selected, ]
-        ret = transcripts[transcripts$gene_id == row$id, ]
-        ss = row$species
-        file = species[species$shortName == ss, ]$file
-        fa = open(FaFile(file))
-        idx = scanFaIndex(fa)
-        seq = sapply(ret$transcript_id, function(n) {
-          as.character(getSeq(fa, idx[seqnames(idx) == n]))
+    server = function(input, output, session) {
+        geneData = reactive({
+            fread('data/genes.csv')
         })
-        cbind(ret, seq)
-      }
-    })
-  }
+        transcriptData = reactive({
+            fread('data/transcripts.csv')
+        })
+        speciesData = reactive({
+            fread('data/species.csv')
+        })
+
+        orthogroupData = reactive({
+            fread('data/orthologs.csv')
+        })
+
+        observe({
+            query <- parseQueryString(session$clientData$url_search)
+            for (i in 1:(length(reactiveValuesToList(input)))) {
+                nameval = names(reactiveValuesToList(input)[i])
+                valuetoupdate = query[[nameval]]
+
+                if (!is.null(query[[nameval]])) {
+                    if (is.na(as.numeric(valuetoupdate))) {
+                        updateTextInput(session, nameval, value = valuetoupdate)
+                    }
+                    else {
+                        updateTextInput(session, nameval, value = as.numeric(valuetoupdate))
+                    }
+                }
+            }
+        })
+        geneTable = reactive({
+            data = geneData()
+            species = speciesData()
+            if (is.null(input$species)) {
+                return(NULL)
+            }
+            if (input$species != "All") {
+                ss = species[species$name == input$species, ]$shortName
+                data = data[data$species == ss, ]
+            }
+            if (input$gene != "") {
+                query = sprintf("select * from data where id LIKE '%%%s%%'", input$gene)
+                data = sqldf(query)
+            }
+            data
+        })
+        orthologTable = reactive({
+            data = orthologData()
+            species = speciesData()
+            if (is.null(input$species)) {
+                return(NULL)
+            }
+            if (input$species != "All") {
+                ss = species[species$name == input$species, ]$shortName
+                data = data[data$species == ss, ]
+            }
+            if (input$gene != "") {
+                query = sprintf("select * from data where id LIKE '%%%s%%'", input$gene)
+                data = sqldf(query)
+            }
+            data
+        })
+        output$table = DT::renderDataTable(geneTable(), selection = 'single')
+
+
+        output$species = renderUI({
+            data = speciesData()
+            selectInput("species", "Species:", c("All", data$name))
+        })
+
+
+        output$row = renderTable({
+            if (!is.null(input$table_rows_selected)) {
+                data = geneTable()
+                species = speciesData()
+                transcripts = transcriptData()
+
+                row = data[input$table_rows_selected, ]
+                ret = transcripts[transcripts$gene_id == row$id, ]
+                ss = row$species
+                file = species[species$shortName == ss, ]$file
+                fa = open(FaFile(file))
+                idx = scanFaIndex(fa)
+                seq = sapply(ret$transcript_id, function(n) {
+                    as.character(getSeq(fa, idx[seqnames(idx) == n]))
+                })
+                cbind(ret, seq)
+            }
+        })
+
+        output$orthoTable = DT::renderDataTable(orthologTable(), selection = 'single')
+    }
 )
