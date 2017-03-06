bcb461ad6a8d5c86ab63de5bceb7bc224b9cb439
Colin <colin.diesh@gmail.com>
2017-03-06 15:48:46 -0500

    Add links to genepage app

--- inst/appdir/page/genepage.R
+++ inst/appdir/page/genepage.R
@@ -3,8 +3,17 @@ genepageUI = function(id) {
     tagList(
         h1("Gene data"),
         p("Search for genes or orthologs in this table, and select them by clicking each row. The selected genes will be added to a 'workplace' that you can do further analysis with."),
-        textInput(ns("ortholog"), "Ortholog")
+        textInput(ns("ortholog"), "Ortholog"),
+        DT::dataTableOutput(ns("species"))
     )
 }
 genepageServer = function(input, output, session) {
+
+    output$species = DT::renderDataTable({
+        con = do.call(RPostgreSQL::dbConnect, dbargs)
+        on.exit(RPostgreSQL::dbDisconnect(con))
+
+        query = sprintf("SELECT ortholog_id, species_id, gene_id FROM orthologs WHERE ortholog_id='%s'", input$ortholog)
+        RPostgreSQL::dbGetQuery(con, query)
+    })
 }
