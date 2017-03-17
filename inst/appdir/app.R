a87f81660f2e266f599f3a3cdf441f7975220c15
Colin <colin.diesh@gmail.com>
2017-03-17 13:38:12 -0400

    Replace quotes

--- inst/appdir/app.R
+++ inst/appdir/app.R
@@ -8,18 +8,18 @@ ui = function(request) {
     source('page/species.R', local = T)
     source('page/recently_updated.R', local = T)
     fluidPage(
-        includeCSS("styles.css"),
+        includeCSS('styles.css'),
         headerPanel('ShinyOrthologs'),
-        wellPanel(style = "background-color: #ffffff;", 
+        wellPanel(style = 'background-color: #ffffff;', 
             tabsetPanel(id = 'inTabset',
-                tabPanel(style = "margin: 20px;", id = 'search', 'Home', searchUI('search')),
-                tabPanel(style = "margin: 20px;", id = 'comparisons', 'Heatmap', comparisonsUI('comparisons')),
-                tabPanel(style = "margin: 20px;", id = 'species', 'Species table', speciesUI('species')),
-                tabPanel(style = "margin: 20px;", id = 'msa', 'MSA', msaUI('msa')),
-                tabPanel(style = "margin: 20px;", id = 'genepage', 'Gene page', genepageUI('genepage')),
-                tabPanel(style = "margin: 20px;", id = 'edit', 'Edit', editUI('edits')),
-                tabPanel(style = "margin: 20px;", id = 'updated', 'Recent updates', updatesUI('updates')),
-                tabPanel(style = "margin: 20px;", id = 'help', 'Help', helpUI('help'))
+                tabPanel(style = 'margin: 20px;', id = 'search', 'Home', searchUI('search')),
+                tabPanel(style = 'margin: 20px;', id = 'comparisons', 'Heatmap', comparisonsUI('comparisons')),
+                tabPanel(style = 'margin: 20px;', id = 'species', 'Species table', speciesUI('species')),
+                tabPanel(style = 'margin: 20px;', id = 'msa', 'MSA', msaUI('msa')),
+                tabPanel(style = 'margin: 20px;', id = 'genepage', 'Gene page', genepageUI('genepage')),
+                tabPanel(style = 'margin: 20px;', id = 'edit', 'Edit', editUI('edits')),
+                tabPanel(style = 'margin: 20px;', id = 'updated', 'Recent updates', updatesUI('updates')),
+                tabPanel(style = 'margin: 20px;', id = 'help', 'Help', helpUI('help'))
             )
         )
     )
@@ -36,46 +36,46 @@ server = function(input, output, session) {
     source('page/recently_updated.R', local = T)
     setBookmarkExclude(
         c(
-            "search-table_rows_current",
-            "search-table_cell_clicked",
-            "search-table_search",
-            "search-table_rows_all",
-            "search-table_state",
-            "species-table_rows_current",
-            "species-table_cell_clicked",
-            "species-table_species",
-            "species-table_rows_selected",
-            "species-table_rows_all",
-            "species-table_state",
-            "species-table_row_last_clicked",
-            "updates-table_rows_current",
-            "updates-table_cell_clicked",
-            "updates-table_species",
-            "updates-table_rows_selected",
-            "updates-table_rows_all",
-            "updates-table_state",
-            "updates-table_row_last_clicked",
-            "edits-table_rows_current",
-            "edits-table_cell_clicked",
-            "edits-table_species",
-            "edits-table_rows_all",
-            "edits-table_state",
-            "edits-table_row_last_clicked",
-            "table_edited_rows_current",
-            "table_edited_cell_clicked",
-            "table_edited_search",
-            "table_edited_rows_selected",
-            "table_edited_rows_all",
-            "table_edited_state",
-            "table_edited_row_last_clicked",
-            "table_removed_rows_current",
-            "table_removed_cell_clicked",
-            "table_removed_search",
-            "table_removed_rows_selected",
-            "table_removed_rows_all",
-            "table_removed_state",
-            "table_removed_row_last_clicked",
-            "genepage-fasta"
+            'search-table_rows_current',
+            'search-table_cell_clicked',
+            'search-table_search',
+            'search-table_rows_all',
+            'search-table_state',
+            'species-table_rows_current',
+            'species-table_cell_clicked',
+            'species-table_species',
+            'species-table_rows_selected',
+            'species-table_rows_all',
+            'species-table_state',
+            'species-table_row_last_clicked',
+            'updates-table_rows_current',
+            'updates-table_cell_clicked',
+            'updates-table_species',
+            'updates-table_rows_selected',
+            'updates-table_rows_all',
+            'updates-table_state',
+            'updates-table_row_last_clicked',
+            'edits-table_rows_current',
+            'edits-table_cell_clicked',
+            'edits-table_species',
+            'edits-table_rows_all',
+            'edits-table_state',
+            'edits-table_row_last_clicked',
+            'table_edited_rows_current',
+            'table_edited_cell_clicked',
+            'table_edited_search',
+            'table_edited_rows_selected',
+            'table_edited_rows_all',
+            'table_edited_state',
+            'table_edited_row_last_clicked',
+            'table_removed_rows_current',
+            'table_removed_cell_clicked',
+            'table_removed_search',
+            'table_removed_rows_selected',
+            'table_removed_rows_all',
+            'table_removed_state',
+            'table_removed_row_last_clicked',
+            'genepage-fasta'
         )
     )
 
@@ -97,4 +97,4 @@ server = function(input, output, session) {
 }
 
 
-shinyApp(ui, server, enableBookmarking = "url")
+shinyApp(ui, server, enableBookmarking = 'url')
