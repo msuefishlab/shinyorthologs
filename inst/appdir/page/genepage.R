f4902bc7966d4c2ae50ec960aee1ddbf59b079a9
Colin <colin.diesh@gmail.com>
2017-04-10 16:01:56 -0400

    Add dropdown for transcripts

--- inst/appdir/page/genepage.R
+++ inst/appdir/page/genepage.R
@@ -2,9 +2,15 @@ genepageUI = function(id) {
     ns = NS(id)
     tagList(
         textInput(ns('ortholog'), 'Ortholog'),
-        DT::dataTableOutput(ns('table')),
-        downloadButton(ns('downloadData'), 'Download all FASTA'),
-        uiOutput(ns('fasta'))
+        h3('Genes'),
+        DT::dataTableOutput(ns('genes')),
+        h3('Transcripts'),
+        HTML('<button data-toggle="collapse" data-target="#transcripts_container">></button>'),
+        tags$div(id = 'transcripts_container',  class="collapse",
+            DT::dataTableOutput(ns('table')),
+            uiOutput(ns('fasta')),
+            downloadButton(ns('downloadData'), 'Download all FASTA')
+        )
     )
 }
 genepageServer = function(input, output, session, box) {
@@ -19,6 +25,19 @@ genepageServer = function(input, output, session, box) {
         rs = dbSendQuery(conn, q)
         dbFetch(rs)
     })
+
+    output$genes = DT::renderDataTable({
+        if(is.null(input$ortholog) || input$ortholog == '') {
+            return()
+        }
+        conn <- poolCheckout(pool)
+        on.exit(poolReturn(conn))
+        query = 'SELECT s.species_id, o.ortholog_id, o.evidence, g.gene_id, od.symbol, od.description FROM orthologs o join genes g on g.gene_id = o.gene_id left join species s on s.species_id = o.species_id left join orthodescriptions od on o.ortholog_id = od.ortholog_id left join dbxrefs db on g.gene_id = db.gene_id where o.ortholog_id = ?orthoid'
+        q = sqlInterpolate(conn, query, orthoid = input$ortholog)
+        rs = dbSendQuery(conn, q)
+        dbFetch(rs)
+    }, selection = 'single')
+
     output$table = DT::renderDataTable({
         dataTable()[,c(2,3,4,5,6,7,8)]
     }, selection = 'single')
