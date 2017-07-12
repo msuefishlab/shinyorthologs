78068a79bcc4e491a5b17bfc192b6bab9341e696
Colin <colin.diesh@gmail.com>
2017-07-12 15:05:12 +0000

    Break app.R into ui/server

--- server.R
+++ server.R
@@ -48,33 +48,7 @@ names(expressionFiles) = files
 pool::poolReturn(conn)
 
 
-shiny::runApp()
-ui = function(request) {
-    source('page/search.R', local = T)
-    source('page/heatmap.R', local = T)
-    source('page/genepage.R', local = T)
-    source('page/help.R', local = T)
-    source('page/edit.R', local = T)
-    source('page/species.R', local = T)
-    source('page/recently_updated.R', local = T)
-    fluidPage(
-        includeCSS('styles.css'),
-        headerPanel('ShinyOrthologs'),
-        wellPanel(style = 'background-color: #ffffff;', 
-            tabsetPanel(id = 'inTabset',
-                tabPanel(style = 'margin: 20px;', id = 'search', 'Home', searchUI('search')),
-                tabPanel(style = 'margin: 20px;', id = 'heatmap', 'Heatmap', heatmapUI('heatmap')),
-                tabPanel(style = 'margin: 20px;', id = 'species', 'Species table', speciesUI('species')),
-                tabPanel(style = 'margin: 20px;', id = 'genepage', 'Gene page', genepageUI('genepage')),
-                tabPanel(style = 'margin: 20px;', id = 'edit', 'Edit', editUI('edits')),
-                tabPanel(style = 'margin: 20px;', id = 'updated', 'Recent updates', updatesUI('updates')),
-                tabPanel(style = 'margin: 20px;', id = 'help', 'Help', helpUI('help'))
-            )
-        )
-    )
-}
-
-server = function(input, output, session) {
+shinyServer(function(input, output, session) {
     source('page/search.R', local = T)
     source('page/heatmap.R', local = T)
     source('page/help.R', local = T)
@@ -146,7 +120,6 @@ server = function(input, output, session) {
     onBookmarked(function(url) {
         updateQueryString(url)
     })
-}
+})
 
-
-shinyApp(ui, server, enableBookmarking = 'url')
+enableBookmarking("url")
---
+++ ui.R
@@ -0,0 +1,25 @@
+library(shiny)
+shinyUI(function(request) {
+    source('page/search.R', local = T)
+    source('page/heatmap.R', local = T)
+    source('page/genepage.R', local = T)
+    source('page/help.R', local = T)
+    source('page/edit.R', local = T)
+    source('page/species.R', local = T)
+    source('page/recently_updated.R', local = T)
+    fluidPage(
+        includeCSS('styles.css'),
+        headerPanel('ShinyOrthologs'),
+        wellPanel(style = 'background-color: #ffffff;', 
+            tabsetPanel(id = 'inTabset',
+                tabPanel(style = 'margin: 20px;', id = 'search', 'Home', searchUI('search')),
+                tabPanel(style = 'margin: 20px;', id = 'heatmap', 'Heatmap', heatmapUI('heatmap')),
+                tabPanel(style = 'margin: 20px;', id = 'species', 'Species table', speciesUI('species')),
+                tabPanel(style = 'margin: 20px;', id = 'genepage', 'Gene page', genepageUI('genepage')),
+                tabPanel(style = 'margin: 20px;', id = 'edit', 'Edit', editUI('edits')),
+                tabPanel(style = 'margin: 20px;', id = 'updated', 'Recent updates', updatesUI('updates')),
+                tabPanel(style = 'margin: 20px;', id = 'help', 'Help', helpUI('help'))
+            )
+        )
+    )
+})
