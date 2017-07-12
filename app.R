916ab3245a50acceec042a237835129768b009b3
Colin <colin.diesh@gmail.com>
2017-07-12 14:47:23 +0000

    Consolidate under app.R

--- app.R
+++ app.R
@@ -1,3 +1,54 @@
+config = jsonlite::fromJSON('config.json')
+dbname = config$dbname
+basedir = config$basedir
+user = config$user
+password = config$password
+port = config$port
+host = config$host
+
+dbargs = c(
+    RPostgreSQL::PostgreSQL(),
+    list(dbname = dbname)[!is.null(dbname)],
+    list(host = host)[!is.null(host)],
+    list(user = user)[!is.null(user)],
+    list(password = password)[!is.null(password)],
+    list(port = port)[!is.null(port)]
+)
+pool = do.call(pool::dbPool, dbargs)
+
+
+## load fasta files for transcriptomes
+fastaIndexes = list()
+conn = pool::poolCheckout(pool)
+query = DBI::dbSendQuery(conn, 'SELECT transcriptome_fasta from species')
+ret = DBI::dbFetch(query)
+fastas = ret$transcriptome_fasta[!is.na(ret$transcriptome_fasta)]
+print(fastas)
+fastaIndexes = lapply(fastas, function(file) {
+    print(file)
+    file = paste0(basedir, file)
+    fa = open(Rsamtools::FaFile(file))
+    Rsamtools::scanFaIndex(fa)
+})
+names(fastaIndexes) = fastas
+
+
+
+## load expression data
+expressionFiles = list()
+query = DBI::dbSendQuery(conn, 'SELECT expression_file from species')
+ret = DBI::dbFetch(query)
+files = ret$expression_file[!is.na(ret$expression_file)]
+expressionFiles = lapply(files, function(expr) {
+    print(expr)
+    expr = paste0(basedir, expr)
+    data.table::fread(expr)
+})
+names(expressionFiles) = files
+pool::poolReturn(conn)
+
+
+shiny::runApp()
 ui = function(request) {
     source('page/search.R', local = T)
     source('page/heatmap.R', local = T)
