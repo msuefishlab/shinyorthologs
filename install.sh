f6013291bca2900b3d7e873c4f257ee4602e2b71
Colin <colin.diesh@gmail.com>
2017-07-18 13:43:04 -0400

    Update install

--- install.sh
+++ install.sh
@@ -1,6 +1,18 @@
 #!/usr/bin/env Rscript
 
 source("https://bioconductor.org/biocLite.R")
-biocLite(c('devtools','tools','data.table','jsonlite','DT','shiny','RPostgreSQL','reshape2','DBI','pheatmap'))
+biocLite(c(
+    'devtools',
+    'tools',
+    'data.table',
+    'jsonlite',
+    'DT',
+    'shiny',
+    'RPostgreSQL',
+    'reshape2',
+    'DBI',
+    'testthat',
+    'pheatmap')
+)
 biocLite('Rsamtools')
 biocLite('rstudio/pool')
