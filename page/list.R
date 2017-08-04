9ce5d87b5136c683ea36aabc970e9cca675e1a38
Colin <colin.diesh@gmail.com>
2017-08-04 04:30:26 +0000

    Create the list widget

--- page/list.R
+++ page/list.R
@@ -2,18 +2,32 @@ listUI = function(id) {
     ns = NS(id)
     tagList(
         textAreaInput(ns('genes'), 'Enter a list of genes and lookup connected ortholog IDs', height = '200px', width = '600px'),
-        uiOutput(ns('results'))
+        DT::dataTableOutput(ns('table'))
     )
 }
-listServer = function(input, output, session) {
-    output$results = renderUI({
+listServer = function(input, output, session, box) {
+    dataTable = reactive({
         if (is.null(input$genes) | input$genes == '') {
             return()
         }
+        x = strsplit(input$genes, '\n')
+        formatted_ids = sapply(x, function(e) {
+            paste0("'", trimws(e), "'")
+        })
+        formatted_list = do.call(paste, c(as.list(formatted_ids), sep = ','))
+        mylist = paste0('(', formatted_list, ')')
+        
+        conn = pool::poolCheckout(pool)
+        on.exit(pool::poolReturn(conn))
 
 
-        tagList(
-            p('test')
-        )
+        query = sprintf('SELECT o.gene_id, o.ortholog_id FROM orthologs o WHERE o.gene_id IN %s', mylist)
+        rs = DBI::dbSendQuery(conn, query)
+        DBI::dbFetch(rs)
     })
+
+    output$table = DT::renderDataTable({
+        dataTable()
+    }, selection = 'single')
+
 }
