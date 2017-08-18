7ee5bc8c24323b2731933293f3af7deb20381fd9
Colin <colin.diesh@gmail.com>
2017-08-18 16:37:16 +0000

    quote string on list panel

--- page/list.R
+++ page/list.R
@@ -13,17 +13,16 @@ listServer = function(input, output, session, parent, heatmap) {
         if (is.null(input$genes) | input$genes == '') {
             return()
         }
-        x = strsplit(input$genes, '\n')
-        formatted_ids = sapply(x, function(e) {
-            paste0("'", trimws(e), "'")
-        })
-        formatted_list = do.call(paste, c(as.list(formatted_ids), sep = ','))
-        mylist = paste0('(', formatted_list, ')')
-        
+ 
         conn = pool::poolCheckout(pool)
         on.exit(pool::poolReturn(conn))
 
-
+        gen = strsplit(input$genes, '\n')
+        gen = sapply(gen, trimws)
+        gen = sapply(gen, function(elt) {
+            dbQuoteString(conn, elt)
+        })
+        mylist = paste0('(', do.call(paste, c(as.list(gen), sep = ',')), ')')
         query = sprintf('SELECT o.gene_id, o.ortholog_id FROM orthologs o WHERE o.gene_id IN %s', mylist)
         rs = DBI::dbSendQuery(conn, query)
         DBI::dbFetch(rs)
